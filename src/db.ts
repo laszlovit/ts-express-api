c61837068fc15f195415328942ace898840b777e
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2024-04-17 12:08:12 +0200

    modify file to resolve merge conflict

--- src/db.ts
+++ src/db.ts
@@ -2,11 +2,12 @@ import { MongoClient } from 'mongodb';
 
 require('dotenv').config();
 
-const MONGO_URI = process.env.NODE_ENV === 'test' 
-  ? process.env.MONGO_URI_TEST  // Use test database URI when in test mode
-  : process.env.MONGO_URI;      // Use production database URI otherwise
+const MONGO_URI =
+  process.env.NODE_ENV === 'test'
+    ? process.env.MONGO_URI_TEST // Use test database URI when in test mode
+    : process.env.MONGO_URI; // Use production database URI otherwise
 
-
+    
 if (!MONGO_URI) {
   throw new Error(
     'MongoDB connection URI is missing in the environment variables');
