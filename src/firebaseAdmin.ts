4458d4fb136e5d17347fec475f693dae3b2976fc
László Vitkai <115879662+laszlovit@users.noreply.github.com>
2024-05-16 10:05:13 +0200

    implemented auth with firebase-admin

---
+++ src/firebaseAdmin.ts
@@ -0,0 +1,17 @@
+import * as admin from 'firebase-admin';
+
+const serviceAccountEnv = process.env.FIREBASE_SERVICE_ACCOUNT;
+
+if (!serviceAccountEnv) {
+  throw new Error('FIREBASE_SERVICE_ACCOUNT environment variable is not set');
+}
+
+const serviceAccount = JSON.parse(
+  Buffer.from(serviceAccountEnv, 'base64').toString('utf-8'),
+);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+export default admin;
